27a69dac9262a7f006d8ed1356df55e3192aa8e0
neeraj <neeraj28@gmail.com>
2025-09-20 08:10:34 +0000

    changes done

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
-
-variable "vpc_id" {}
-variable "private_subnet_ids" {
-  type = list(string)
-}
